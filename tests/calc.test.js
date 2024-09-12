8ef2d1d28f80c9b92edac0078668fddee5e8bebc
Noah Davis Reynolds <Noahdavisreynolds@hotmail.com>
2024-09-12 10:19:31 +0000

    finished

---
+++ tests/calc.test.js
@@ -0,0 +1,21 @@
+const addition = require("../calc");
+
+describe("Calculator", () => {
+    describe("Addition function", () => {
+        test("should return 42 for 20 + 22", () => {
+            expect(addition(20, 22)).toBe(42);
+        });
+        test("should return 73 for 31 + 42", () => {
+            expect(addition(31, 42)).toBe(73);
+        });
+    });
+    describe("Subtract function", () => {
+
+    });
+    describe("Multiply function", () => {
+
+    });
+    describe("Divide function", () => {
+
+    });
+});
